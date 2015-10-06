7c2f95318aa1823b89a3f9a0b8c05c1d516d279f
Jesús Marcos <jesusmarcoscorbacho@gmail.com>
2015-10-06 16:57:17 +0200

    Creado y subido el formulario

---
+++ formulario.html.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+		<meta charset="UTF-8">
+		<title>Cuenta Bancaria</title>
+</head>
+<body>
+		<h1>Introduzca su numero de cuenta</h1>
+		<form action="" method="POST"> 
+				<div>
+						<label for="entidad">Entidad:	
+								<input type="text" name="entidad" required>
+						</label>
+				</div>
+				<div>
+						<label for="oficina">Oficina:
+								<input type="text" name="oficina" required>
+						</label>
+				</div>
+				<div>
+						<label for="digitodecontrol">Digito de Control:
+								<input type="text" name="digitodecontrol" required>
+						</label>
+				</div>
+				<div>
+						<label for="numerodecuenta">Numero de Cuenta
+								<input type="text" name="numerodecuenta" required>
+						</label>
+				</div>
+				<br>
+				<div>
+						<input type="submit" value="Acceder">	
+				</div>
+		</form>
+</body>
+</html>
